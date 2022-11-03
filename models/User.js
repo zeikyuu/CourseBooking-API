8f6c45cb44f9fdd79bfa9cc66366fa77d0df7e39
zeikyuu <zedrick.velasquez07@gmail.com>
2022-11-03 11:33:26 +0800

    Discussion

--- models/User.js
+++ models/User.js
@@ -1,47 +1,48 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
-		reuired: [ true, "FirstName is Required!"]
-	},
-	lastName: {
-		type: String,
-		reuired: [ true, "LastName is Required!"]
-	},
-	email: {
-		type: String,
-		reuired: [ true, "Email is Required!"]
-	},
-	password: {
-		type: String,
-		reuired: [ true, "Password is Required!"]
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false
-	},
-	mobileNo: {
-		type: String,
-		required: [true, "Mobile Number is Required!"]
-	},
-	enrollments: [{
-		courseId: {
-			type: String,
-			reuired: [true, "CourseId is required!"]
-		},
-		enrolledOn: {
-			type: Date,
-			default: new Date()
-		},
-		status: {
-			type: String,
-			default: "Enrolled"
-		}
-	}]
+		const userSchema = new mongoose.Schema({
+			firstName : {
+				type : String,
+				required : [true, "First name is required"]
+			},
+			lastName : {
+				type : String,
+				required : [true, "Last name is required"]
+			},
+			email : {
+				type : String,
+				required : [true, "Email is required"]
+			},
+			password : {
+				type : String,
+				required : [true, "Password is required"]
+			},
+			isAdmin : {
+				type : Boolean,
+				default : false
+			},
+			mobileNo : {
+				type : String, 
+				required : [true, "Mobile No is required"]
+			},
+			// The "enrollments" property/field will be an array of objects containing the course IDs, the date and time that the user enrolled to the course and the status that indicates if the user is currently enrolled to a course
+			enrollments : [
+				{
+					courseId : {
+						type : String,
+						required : [true, "Course ID is required"]
+					},
+					enrolledOn : {
+						type : Date,
+						default : new Date()
+					},
+					status : {
+						type : String,
+						default : "Enrolled"
+					}
+				}
+			]
+		})
 
+		module.exports = mongoose.model("User", userSchema);
 
-
-})
-
-module.exports = mongoose.model("User", userSchema);
